57eaea3e1859a2f34f498c406cdca32e510e2f9b
EMI-Indonesia <eventmanajamenindonesia@gmail.com>
2026-04-28 19:12:18 +0700

    Upload

---
+++ GJS 360 - 2026/app-files/data.js
@@ -0,0 +1,794 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-02_gjs-gerbang-2_result",
+      "name": "02_GJS Gerbang 2_result",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.3164323570858265,
+        "pitch": -0.04055292560724766,
+        "fov": 1.34561329314477
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2501531727300694,
+          "pitch": -0.054415699699070785,
+          "rotation": 0,
+          "target": "4-06_gjs-jalur-masuk-vip_1_result"
+        },
+        {
+          "yaw": 1.1305057132120773,
+          "pitch": -0.016512092704227044,
+          "rotation": 0,
+          "target": "1-03_gjs-jalur-masuk-bus-1_result"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-03_gjs-jalur-masuk-bus-1_result",
+      "name": "03_GJS Jalur Masuk Bus 1_result",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.765585580925652,
+        "pitch": 0.0018846124553846266,
+        "fov": 1.34561329314477
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-04_gjs-jalur-masuk-bus-2_result",
+      "name": "04_GJS Jalur Masuk Bus 2_result",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.320296007520211,
+          "pitch": 0.01886587092642955,
+          "rotation": 1.5707963267948966,
+          "target": "7-09_gjs-jalur-masuk-vip_4_result"
+        },
+        {
+          "yaw": 0.24756976144202092,
+          "pitch": 0.03174851255878508,
+          "rotation": 0,
+          "target": "3-05_gjs-jalur-masuk-bus-3_result"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-05_gjs-jalur-masuk-bus-3_result",
+      "name": "05_GJS Jalur Masuk Bus 3_result",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.56462690780101,
+          "pitch": 0.08315253081643093,
+          "rotation": 1.5707963267948966,
+          "target": "2-04_gjs-jalur-masuk-bus-2_result"
+        },
+        {
+          "yaw": 1.9258415581399326,
+          "pitch": 0.05366325748990519,
+          "rotation": 1.5707963267948966,
+          "target": "8-10_gjs-jalur-masuk-vip_5_result"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-06_gjs-jalur-masuk-vip_1_result",
+      "name": "06_GJS Jalur Masuk VIP_1_result",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07244836795764797,
+          "pitch": -0.01138372038963098,
+          "rotation": 0,
+          "target": "5-07_gjs-jalur-masuk-vip_2_result"
+        },
+        {
+          "yaw": 2.6209249497415064,
+          "pitch": 0.029014754058332315,
+          "rotation": 0,
+          "target": "0-02_gjs-gerbang-2_result"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-07_gjs-jalur-masuk-vip_2_result",
+      "name": "07_GJS Jalur Masuk VIP_2_result",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.8164360593448023,
+        "pitch": -0.0244999619200037,
+        "fov": 1.34561329314477
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7841498884454108,
+          "pitch": 0.019585060474327065,
+          "rotation": 0,
+          "target": "6-08_gjs-jalur-masuk-vip_3_result"
+        },
+        {
+          "yaw": -3.058865129630391,
+          "pitch": 0.039846210226698986,
+          "rotation": 0,
+          "target": "4-06_gjs-jalur-masuk-vip_1_result"
+        },
+        {
+          "yaw": 0.2418079539544653,
+          "pitch": -0.022440241092350988,
+          "rotation": 0,
+          "target": "7-09_gjs-jalur-masuk-vip_4_result"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-08_gjs-jalur-masuk-vip_3_result",
+      "name": "08_GJS Jalur Masuk VIP_3_result",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4734224029339869,
+          "pitch": 0.03338012366738141,
+          "rotation": 0,
+          "target": "5-07_gjs-jalur-masuk-vip_2_result"
+        },
+        {
+          "yaw": -1.6948066823898493,
+          "pitch": 0.12204536882614292,
+          "rotation": 6.283185307179586,
+          "target": "2-04_gjs-jalur-masuk-bus-2_result"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-09_gjs-jalur-masuk-vip_4_result",
+      "name": "09_GJS Jalur Masuk VIP_4_result",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -3.0746100150917854,
+        "pitch": 0.09622609478083177,
+        "fov": 1.34561329314477
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2606493234144454,
+          "pitch": 0.08728167394990649,
+          "rotation": 0.7853981633974483,
+          "target": "9-11_gjs-jalur-masuk-vip_6_-penonton-utara_result"
+        },
+        {
+          "yaw": -2.1415855550521012,
+          "pitch": 0.04867963614590742,
+          "rotation": 5.497787143782138,
+          "target": "8-10_gjs-jalur-masuk-vip_5_result"
+        },
+        {
+          "yaw": -0.16573265360985978,
+          "pitch": 0.11202864382189404,
+          "rotation": 0,
+          "target": "5-07_gjs-jalur-masuk-vip_2_result"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-10_gjs-jalur-masuk-vip_5_result",
+      "name": "10_GJS Jalur Masuk VIP_5_result",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.16328724702936448,
+        "pitch": -0.07538449821538507,
+        "fov": 1.34561329314477
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4129307046848343,
+          "pitch": 0.07925437120130319,
+          "rotation": 0,
+          "target": "3-05_gjs-jalur-masuk-bus-3_result"
+        },
+        {
+          "yaw": -1.7593699534705252,
+          "pitch": 0.03751562511364703,
+          "rotation": 0.7853981633974483,
+          "target": "7-09_gjs-jalur-masuk-vip_4_result"
+        },
+        {
+          "yaw": -0.1627621544964697,
+          "pitch": -0.08713663136536631,
+          "rotation": 0,
+          "target": "15-17_loby-vvip_result"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-11_gjs-jalur-masuk-vip_6_-penonton-utara_result",
+      "name": "11_GJS Jalur Masuk VIP_6_ Penonton Utara_result",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.8582701579394794,
+        "pitch": 0.0452306989292417,
+        "fov": 1.34561329314477
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2580145211356966,
+          "pitch": 0.08359050757751518,
+          "rotation": 4.71238898038469,
+          "target": "8-10_gjs-jalur-masuk-vip_5_result"
+        },
+        {
+          "yaw": -1.2768378198041095,
+          "pitch": 0.06735309691896418,
+          "rotation": 0,
+          "target": "10-12_gjs-jalur-masuk-vip_7_penonton-utara_result"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-12_gjs-jalur-masuk-vip_7_penonton-utara_result",
+      "name": "12_GJS Jalur Masuk VIP_7_Penonton Utara_result",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.4169570024819382,
+        "pitch": -0.03203841174154576,
+        "fov": 1.34561329314477
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4172705675638166,
+          "pitch": 0.0632809661420346,
+          "rotation": 0,
+          "target": "9-11_gjs-jalur-masuk-vip_6_-penonton-utara_result"
+        },
+        {
+          "yaw": -1.4706283017003656,
+          "pitch": 0.038964884288503754,
+          "rotation": 0,
+          "target": "11-13_gjs-jalur-masuk-vip_8_penonton-utara_result"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-13_gjs-jalur-masuk-vip_8_penonton-utara_result",
+      "name": "13_GJS Jalur Masuk VIP_8_Penonton Utara_result",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.376471008016157,
+        "pitch": 0.020730737009230893,
+        "fov": 1.34561329314477
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-13_gjs-tribun-utara_1_result",
+      "name": "13_GJS Tribun Utara_1_result",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.23885820433222094,
+        "pitch": -0.007543735275135077,
+        "fov": 1.34561329314477
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0624603734967604,
+          "pitch": 0.0279579039831912,
+          "rotation": 0,
+          "target": "11-13_gjs-jalur-masuk-vip_8_penonton-utara_result"
+        },
+        {
+          "yaw": -0.9330878251899524,
+          "pitch": 0.006209269162463116,
+          "rotation": 0,
+          "target": "13-14_gjs-tribun-utara_2_result"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-14_gjs-tribun-utara_2_result",
+      "name": "14_GJS Tribun Utara_2_result",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.30768023004514333,
+        "pitch": -0.018846124553846266,
+        "fov": 1.34561329314477
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2089874145893234,
+          "pitch": 0.0246923484326107,
+          "rotation": 0,
+          "target": "12-13_gjs-tribun-utara_1_result"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-15_gjs-akses-pemain_result",
+      "name": "15_GJS Akses Pemain_result",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0859063367358086,
+          "pitch": 0.016623006768284654,
+          "rotation": 0,
+          "target": "16-18_loby-vvip_2_result"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-17_loby-vvip_result",
+      "name": "17_Loby VVIP_result",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-18_loby-vvip_2_result",
+      "name": "18_Loby VVIP_2_result",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.09986162215019334,
+        "pitch": 0.009423062276923133,
+        "fov": 1.34561329314477
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9712481834458195,
+          "pitch": 0.09528139856998408,
+          "rotation": 0,
+          "target": "15-17_loby-vvip_result"
+        },
+        {
+          "yaw": 0.1309360401547437,
+          "pitch": 0.03318159359828243,
+          "rotation": 0,
+          "target": "14-15_gjs-akses-pemain_result"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
